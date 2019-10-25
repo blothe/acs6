e0cc2124d8d3de5c5f2f21442d62d2561c8ef7ad
Solange Harmonie PICARD <solange.picard@cryptolab.net>
2019-10-25 14:58:38 +0200

    add icons

--- Components/filter.js
+++ Components/filter.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, TextInput, StyleSheet} from 'react-native'
+import { View, Text, TextInput, StyleSheet, TouchableOpacity} from 'react-native'
 // import InfoIcon from '@material-ui/icons/Info';
 
 class Filter extends React.Component {
@@ -7,12 +7,22 @@ class Filter extends React.Component {
     return (
       <View style={styles.main_filter}>
         <View style={styles.content_title}>
-          <Text>J'affine ma recherche</Text>
+          <Text style={styles.text}>J'affine ma recherche</Text>
         </View>
-        <View>
-          <TextInput placeholder="recherche"/>
+        <View style={styles.content_searchb}>
+          <TextInput style={styles.searchb} placeholder="recherche"/>
         </View>
-        <View>
+        <View style={styles.content_categories}>
+          <TouchableOpacity style={styles.btn}>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btn}>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btn}>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btn}>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btn}>
+          </TouchableOpacity>
         </View>
 
       </View>
@@ -22,10 +32,47 @@ class Filter extends React.Component {
 
 const styles = StyleSheet.create({
   main_filter: {
-    backgroundColor: "#0a2342"
+    backgroundColor: "#0a2342",
+    flex: 1
   },
   content_title: {
+    color: 'white',
+    justifyContent: "center"
+  },
+  text: {
+    color: "white",
+    marginTop: 30,
+    fontSize: 24,
+    textAlign: "center"
 
+  },
+  content_searchb: {
+    paddingTop: 20,
+    flexDirection: "row",
+    justifyContent: "center"
+  },
+  searchb: {
+    height: 45,
+    width: 300,
+    paddingTop: 10,
+    backgroundColor: "white",
+    borderRadius: 20,
+
+  },
+  content_categories: {
+    marginTop: 40,
+    height: 70,
+    width: 350,
+    backgroundColor: "white",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  btn: {
+    height: 60,
+    width: 60,
+    borderRadius: 50,
+    backgroundColor: "pink"
   }
-})
+});
 export default Filter
