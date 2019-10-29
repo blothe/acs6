27b12a339d56a987d28cc959b5f7d2aa2bc76b5f
Solange Harmonie PICARD <solange.picard@cryptolab.net>
2019-10-29 10:15:08 +0100

    integrate react native map

--- Components/FilterMap.js
+++ Components/FilterMap.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import MapView from 'react-native-maps';
-import { View, StyleSheet} from 'react-native'
+import { View, StyleSheet, Dimensions} from 'react-native'
 
 class FilterMap extends React.Component {
+  getInitialState() {
+    return {
+      region: {
+        latitude: 46.9861191,
+        longitude: 3.1422382,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      },
+    };
+  }
+  onRegionChange(region) {
+    this.setState({ region });
+  }
+
   render() {
     return (
       <View style={styles.container}>
+        <MapView style={styles.mapStyle} region={this.setState} onRegionChange={this.onRgionChange}/>
       </View>
     )
   }
@@ -14,7 +29,11 @@ class FilterMap extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "blue"
+    backgroundColor: "#ffff"
+  },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height
   }
 })
 
