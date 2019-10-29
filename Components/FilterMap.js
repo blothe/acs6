6c241dd5e91c28e390b8dce8be2cb7a63a0f08c3
Solange Harmonie PICARD <solange.picard@cryptolab.net>
2019-10-29 11:25:40 +0100

    map location

--- Components/FilterMap.js
+++ Components/FilterMap.js
@@ -3,24 +3,19 @@ import MapView from 'react-native-maps';
 import { View, StyleSheet, Dimensions} from 'react-native'
 
 class FilterMap extends React.Component {
-  getInitialState() {
-    return {
-      region: {
-        latitude: 46.9861191,
-        longitude: 3.1422382,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      },
-    };
-  }
-  onRegionChange(region) {
-    this.setState({ region });
-  }
-
+  //position initial sur la carte
   render() {
     return (
       <View style={styles.container}>
-        <MapView style={styles.mapStyle} region={this.setState} onRegionChange={this.onRgionChange}/>
+        <MapView
+          style={styles.mapStyle}
+          initialRegion={{
+          latitude: 46.9861191,
+          longitude: 3.1422382,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+          }}
+     />
       </View>
     )
   }
